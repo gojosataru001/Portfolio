1af6be5b6f431bb9d4b9ec24fe6aa332238f3b42
gojosataru001 <sharmahunny16197@gmail.com>
2025-07-17 16:09:37 +0530

    all done may be

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: '/form/', 
   plugins: [react(),
     tailwindcss(),
   ],
-    resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-
+  base: '/portfolio/', // 👈 Make sure this matches your GitHub repo name
 })
